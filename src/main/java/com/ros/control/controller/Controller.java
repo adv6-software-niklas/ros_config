708b8ae88718ffa5c55d3be557f6faa5af25ede2
Robben,Nik Robben N.M.H <nikrobben.robben@student.fontys.nl>
2024-01-21 18:05:03 +0100

    ??

--- src/main/java/com/ros/control/controller/Controller.java
+++ src/main/java/com/ros/control/controller/Controller.java
@@ -2,12 +2,14 @@ package com.ros.control.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ros.control.entity.RosNetworkEntity;
 import com.ros.control.service.RepositoryService;
 
 @RestController
+@RequestMapping("config")
 public class Controller {
     
     private final RepositoryService myServiceBaby;
